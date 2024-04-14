545432eda2a687f79696fa0c395702b1e85a674a
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2024-04-14 16:26:22 +0700

    ...

---
+++ NETCases/HybridNETCases/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace HybridNETCases;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ NETCases/HybridNETCases/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HybridNETCases;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
